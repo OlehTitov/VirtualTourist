3259cded68ee64e17a997964cb46d8439fd3bb71
OlehTitov <oleg.titov81@gmail.com>
2020-07-21 12:25:05 +0300

    initial commit

---
+++ virtualTourist/virtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  virtualTourist
+//
+//  Created by Oleh Titov on 21.07.2020.
+//  Copyright © 2020 Oleh Titov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
