c8a8757b7ed942a684f0d0fb5ef2dfcff48e953a
OlehTitov <oleg.titov81@gmail.com>
2020-07-21 17:42:54 +0300

    add MapVC

---
+++ virtualTourist/virtualTourist/Controller/MapVC.swift
@@ -0,0 +1,18 @@
+//
+//  MapVC.swift
+//  virtualTourist
+//
+//  Created by Oleh Titov on 21.07.2020.
+//  Copyright © 2020 Oleh Titov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class MapVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- virtualTourist/virtualTourist/Controller/tabBarVC.swift
+++ virtualTourist/virtualTourist/Controller/tabBarVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class tabBarVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
