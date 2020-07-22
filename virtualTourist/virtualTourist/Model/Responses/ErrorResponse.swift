e9e63daa9665f001001c8e72713a97f1bf4de74f
OlehTitov <oleg.titov81@gmail.com>
2020-07-22 22:04:04 +0300

    add PhotosSearchResponse struct

--- virtualTourist/virtualTourist/Model/Responses/ErrorResponse.swift
+++ virtualTourist/virtualTourist/Model/Responses/ErrorResponse.swift
@@ -13,3 +13,9 @@ struct ErrorResponse: Codable {
     let code: Int
     let message: String
 }
+
+extension ErrorResponse: LocalizedError {
+    var errorDescription: String? {
+        return message
+    }
+}
---
+++ virtualTourist/virtualTourist/Model/Responses/PhotosSearchResponse.swift
@@ -0,0 +1,34 @@
+//
+//  PhotosSearchResponse.swift
+//  virtualTourist
+//
+//  Created by Oleh Titov on 22.07.2020.
+//  Copyright © 2020 Oleh Titov. All rights reserved.
+//
+
+import Foundation
+
+struct PhotosSearchResponse: Codable {
+    let photos: Photos
+    let stat: String
+}
+
+struct Photos: Codable {
+    let page: Int
+    let pages: Int
+    let perpage: Int
+    let total: String
+    let photo: [Photo]
+}
+
+struct Photo: Codable {
+    let id: String
+    let owner: String
+    let secret: String
+    let server: String
+    let farm: Int
+    let title: String
+    let ispublic: Int
+    let isfriend: Int
+    let isfamily: Int
+}
