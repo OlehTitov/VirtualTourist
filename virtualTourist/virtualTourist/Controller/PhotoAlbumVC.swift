bd3cf9da230e5e9c580918199ee154ccbfdc2c9c
OlehTitov <oleg.titov81@gmail.com>
2020-07-24 06:39:50 +0300

    add PhotoAlbumVC

---
+++ virtualTourist/virtualTourist/Controller/PhotoAlbumVC.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoAlbumVC.swift
+//  virtualTourist
+//
+//  Created by Oleh Titov on 24.07.2020.
+//  Copyright © 2020 Oleh Titov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+import CoreData
+
+class PhotoAlbumVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
