099dc48071258b3e56cb09bc34fad2ae9ede70fd
OlehTitov <oleg.titov81@gmail.com>
2020-07-24 14:19:04 +0300

    add diffable data source to collection view

--- virtualTourist/virtualTourist/Controller/PhotoAlbumVC.swift
+++ virtualTourist/virtualTourist/Controller/PhotoAlbumVC.swift
@@ -13,25 +13,48 @@ import CoreData
 
 class PhotoAlbumVC: UIViewController {
     
+    //MARK: - PROPERTIES
+    enum Section {
+        case main
+    }
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var altitude: CLLocationDistance!
+    var annotation : MKAnnotation!
+    
+    //MARK: - OUTLETS
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var altitude: CLLocationDistance!
-    var annotation : MKAnnotation!
-    
+    //MARK: - VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMapView()
-        
+        configureDataSource()
     }
     
+    //MARK: - METHODS
     func setupMapView() {
         mapView.delegate = self
         mapView.addAnnotation(annotation)
         let region = MKCoordinateRegion(center: annotation.coordinate, span: mapView.region.span)
         mapView.setRegion(region, animated: true)
         mapView.camera.altitude = altitude
+    }
+    
+    private func configureDataSource() {
+        let dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: CollectionViewCell.identifier,
+                for: indexPath) as? CollectionViewCell else { fatalError("Cannot create new cell") }
+
+            // Populate the cell with image
+            //cell.image.image = "\(identifier)"
+
+            return cell
+        }
     }
     
 }
